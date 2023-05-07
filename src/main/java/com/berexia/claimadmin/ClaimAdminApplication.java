6307fe2cef3d568dc7fa8b57baba3323af23b8ac
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-07 18:40:49 +0000

    update claim admin ...

---
+++ src/main/java/com/berexia/claimadmin/ClaimAdminApplication.java
@@ -0,0 +1,13 @@
+package com.berexia.claimadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berexia/claimadmin/ClaimAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berexia.claimadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
